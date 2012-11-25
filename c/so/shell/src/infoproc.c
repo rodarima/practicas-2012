afcaa607ae0f1f1e68307707f5c0edd9e9e866b8
Rodrigo <rodarima@gmail.com>
2012-11-25 17:45:48 +0000

    rusage pro!

--- c/so/shell/src/infoproc.c
+++ c/so/shell/src/infoproc.c
@@ -9,21 +9,42 @@ int infoproc(struct proc_t *p)
 	proc_refresh(p);
 	
 	tm_tiempo = localtime(&(p->time));
-	strftime(time_string, 
+	strftime (time_string, 
 		sizeof(time_string), 
 		"%d %b %Y %H:%M:%S", 
 		tm_tiempo
-		);
-		
-	printf("PID   PRI STAT TIME          CMD\n");
-	printf("%5d %3d %4s %lu %s\n", 
+	);
+
+	printf("PID   PRI STAT TIME                  CMD\n");
+
+	printf("%5d %3d %4s %-21s %s\n", 
 		p->pid, 
 		p->prio, 
 		GETPROCSTATUS(p->status), 
 		time_string, 
 		p->cmd
 	);
-	
+
+	struct rusage *r = &(p->ru);
+	printf("\n");
+	printf("User CPU:	%ld.%06ld\n", r->ru_stime.tv_sec, r->ru_stime.tv_usec);
+	printf("System CPU:	%ld.%06ld\n", r->ru_utime.tv_sec, r->ru_utime.tv_usec);
+	printf("\n");
+	printf("Max. resident set size: %ld\n", r->ru_maxrss);
+	printf("Shared memory size: 	%ld\n", r->ru_ixrss);
+	printf("Unshared data size: 	%ld\n", r->ru_idrss);
+	printf("Unshared stack size:	%ld\n", r->ru_isrss);
+	printf("Page reclaims:		%ld\n", r->ru_minflt);
+	printf("Page faults:		%ld\n", r->ru_majflt);
+	printf("Swaps:			%ld\n", r->ru_nswap);
+	printf("Block input ops.:	%ld\n", r->ru_inblock);
+	printf("block output ops.:	%ld\n", r->ru_oublock);
+	printf("IPC messages sent:	%ld\n", r->ru_msgsnd);
+	printf("IPC messages received:	%ld\n", r->ru_msgrcv);
+	printf("Signals received:	%ld\n", r->ru_nsignals);
+	printf("Vol. context switches:	%ld\n", r->ru_nvcsw);
+	printf("Inv. context switches:	%ld\n", r->ru_nivcsw);
+
 	return 0;
 }
 
